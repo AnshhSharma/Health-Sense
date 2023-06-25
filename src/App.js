c8911f048efea395a5e6ab032de2c23e37810ade
Yash2025 <yash.goyal2025@gmail.com>
2023-06-25 10:58:28 +0530

    Added routing link to about page, and keys disclosed for superiors to access the site

--- src/App.js
+++ src/App.js
@@ -11,9 +11,15 @@ import Footer from './Components/Footer';
 import Profile from './Components/Profile';
 import ChatBot from './Components/ChatBot';
 import LabTest from './Components/LabTest';
+import About from './Components/About';
 function App() {
-  const chatBotKey1 = process.env.REACT_APP_CHAT_BOT_KEY_1;
-  const chatBotKey2 = process.env.REACT_APP_CHAT_BOT_KEY_2;
+  // Do this when we need to hide the key by putting keys in .env.local file
+  // const chatBotKey1 = process.env.REACT_APP_CHAT_BOT_KEY_1;
+  // const chatBotKey2 = process.env.REACT_APP_CHAT_BOT_KEY_2;
+
+  // to show the website work to authorities, we are putting key publicly
+  const chatBotKey1 = 'BjM2mqNevak.JsTcrneqHcyMk0zQSilp7fjEokWOwuUAcCJRNHhtSYM';
+  const chatBotKey2 = 'BjM2mqNevak.beKsl2bFKSoVaV7u3YeiFz989h2Wr4iWKQzc1KhBCkU';
   return (
     <div className="App">
       <Router>
@@ -25,6 +31,7 @@ function App() {
             <Route exact path="/profile" element={<Profile/>} />
             <Route exact path="/chatbot" element={<ChatBot key1 = {chatBotKey1} key2 = {chatBotKey2}/>} />
             <Route exact path="/labtest" element={<LabTest/>} />
+            <Route exact path="/about" element={<About/>} />
           </Routes>
           <Footer/>
         </Router>
